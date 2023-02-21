5145fe57a5d65ad28ba342062966eafd410f35e4
Kori Njoroge <korinjoroge63@gmail.com>
2023-02-22 00:02:39 +0300

    navbar component and styles

--- hombker/src/components/navbar.jsx
+++ hombker/src/components/navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
+import '../styles/navbar.css'
 
 export default function Navbar() {
     return (
         <div className='navbar'>
             <div className="navigation">
-                <div className="navleft">
-                <h4>Hombekr</h4>
+                <div className="navLeft">
+                    <h4><span>H</span>ombekr</h4>
                 </div>
                 <div className="navRight">
                     <p><i className="fa-solid fa-house"></i>Dashboard</p>
                     <p><i className="fa-solid fa-bag-shopping"></i>orders</p>
                     <p><i className="fa-solid fa-user"></i>customize</p>
-                    <p>welcome Joanina's <i className="fa-solid fa-user"></i></p>
                     <p className='vertical'></p>
+                    <p>welcome Joanina's <i className="fa-solid fa-user"></i></p>
                     <p><i className="fa-solid fa-caret-down"></i></p>
                 </div>
             </div>
             <div className="dashboard">
-                <p className="dashboard">Dashboard</p>
+                <p className="dashboardHeading">Dashboard</p>
                 <button className="newOrder"><i className="fa-solid fa-plus"></i> New Order</button>
             </div>
         </div>
