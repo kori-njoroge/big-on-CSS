317c6b682e7d39a474f5d46a2ebf2d2adcac3aa4
Kori Njoroge <korinjoroge63@gmail.com>
2023-02-21 23:20:54 +0300

    react app sctructure, components,styles

--- hombker/src/components/navbar.jsx
+++ hombker/src/components/navbar.jsx
@@ -4,9 +4,22 @@ export default function Navbar() {
     return (
         <div className='navbar'>
             <div className="navigation">
+                <div className="navleft">
                 <h4>Hombekr</h4>
+                </div>
+                <div className="navRight">
+                    <p><i className="fa-solid fa-house"></i>Dashboard</p>
+                    <p><i className="fa-solid fa-bag-shopping"></i>orders</p>
+                    <p><i className="fa-solid fa-user"></i>customize</p>
+                    <p>welcome Joanina's <i className="fa-solid fa-user"></i></p>
+                    <p className='vertical'></p>
+                    <p><i className="fa-solid fa-caret-down"></i></p>
+                </div>
             </div>
-            <div className="dashboard"></div>
+            <div className="dashboard">
+                <p className="dashboard">Dashboard</p>
+                <button className="newOrder"><i className="fa-solid fa-plus"></i> New Order</button>
+            </div>
         </div>
     )
 }
