5fb96b6a4ba7d8dd5fb96b69bdca3a079de197ac
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-01 20:36:52 +0300

    creating cards and styling

--- cssbattle/src/components/body.jsx
+++ cssbattle/src/components/body.jsx
@@ -23,7 +23,40 @@ export default function Body() {
             </div>
             <h4>Our Courses</h4>
             <div className="ourCourses">
-
+                <div className="card">
+                    <div className="cardtop ">
+                        <h3>for beginers</h3>
+                        <p>Learn about the basics of CSS and the most commonly used properties.</p>
+                    </div>
+                    <h5>course stats</h5>
+                    <div className="cardBottom">
+                        <p>70+ exciting levels!</p>
+                        <p>Have a look</p>
+                        <button className='buybuy'>Buy Now! - US#4.63</button>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="cardtop topper">
+                        <h3>for intermediates</h3>
+                        <p>Move a step forward and become a master of layouts.</p>
+                    </div>
+                    <h5>course stats</h5>
+                    <div className="cardBottom">
+                        <p className='announce'>to be announced</p>
+                        <button className='notifyme'>notify me</button>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="cardtop topper">
+                        <h3>for advanced</h3>
+                        <p>Learn about some advanced CSS concepts, animations and how to use them.</p>
+                    </div>
+                    <h5>course stats</h5>
+                    <div className="cardBottom">
+                        <p className='announce'>to be announced</p>
+                        <button className='notifyme'>notify me</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
