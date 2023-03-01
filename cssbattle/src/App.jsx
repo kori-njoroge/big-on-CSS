9be273c43ae352d5ecbc07385fbf5f299e101a60
Kori Njoroge <korinjoroge63@gmail.com>
2023-03-01 18:34:42 +0300

    navbar items created and styles

--- cssbattle/src/App.jsx
+++ cssbattle/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Body from './components/body';
 import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Body />
     </div>
   );
 }
--- cssbattle/src/components/navbar.jsx
+++ cssbattle/src/components/navbar.jsx
@@ -5,12 +5,12 @@ export default function Navbar() {
     return (
         <div className='navbar'>
             <div className="navigation">
-                <h4>CSSBattle</h4>
+                <h4>&#123;X&#125;CSSBattle</h4>
                 <h5>/</h5>
-                <h5>Learn</h5>
+                <h5 id='learn'>Learn</h5>
             </div>
             <div className="navRight">
-                <button>Sign In / Sign Up</button>
+                <button>Sign In &nbsp; / &nbsp;Sign Up</button>
             </div>
         </div>
     )
